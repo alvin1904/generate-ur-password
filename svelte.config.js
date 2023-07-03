aa372077aa7de028c8e19cfe6012084b5214e438
alvin <alvin19official@gmail.com>
2023-07-03 12:49:20 +0530

    add changes to ip & host prod

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,11 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	server: {
+		host: '0.0.0.0',
+		port: 5000
+	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
